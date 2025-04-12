8785e825c54b6cc89bae6c90622d13879b8acc0b
erichsantiago <erichsantiago@outlook.com.br>
2025-04-12 19:24:02 -0300

    Configurações iniciais do projeto, estrutura de pastas e testes para segurança de rota

---
+++ src/app/(public)/sign-in/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Login() {
+    return (
+        <div className="w-screen h-screen bg-white">
+            <div className="flex flex-row justify-center items-center">
+                <div className="w-[390px] text-black">
+                    <h1>Imagem</h1>
+                </div>
+                <div className="w-[390px] text-black">
+                    <h1>Imagem</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
